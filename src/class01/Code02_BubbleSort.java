e34cc6f14b1c0b350f11d7d8cdccfec4ade7271b
yanghm55 <yhm,199503>
2022-05-31 19:13:45 +0800

    init

--- src/class01/Code02_BubbleSort.java
+++ src/class01/Code02_BubbleSort.java
@@ -9,5 +9,26 @@ public class Code02_BubbleSort {
 
     public static void main(String[] args) {
 
+        int[] arr = new int[]{6,3,1,5,7,9,2};
+        bubbleSort(arr);
+        System.out.print(arr);
+
+    }
+
+    // [ 6 5 4 3 2 1]
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0;i < arr.length-1;i++) {
+            for (int j= i+1;j<arr.length;j++) {
+                if (arr[j]>arr[i]) {
+                    swap(arr,i,j);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr,int i,int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
     }
 }
