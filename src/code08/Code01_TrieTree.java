4fa841ec5687bdc51697fddea50945045e605dd2
杨行满 <yanghm55@midea.com>
2022-07-12 20:00:21 +0800

    trie

--- src/code08/Code01_TrieTree.java
+++ src/code08/Code01_TrieTree.java
@@ -10,9 +10,9 @@ import class03.Code03_StackAndQueueImpl;
 
 /**
  * 前缀树
- *
+ * <p>
  * 给定一个字符串数组
- *
+ * <p>
  * 1.遍历每一个字符串
  * 2.遍历每一个字符，如果在前缀树上有值的话复用，没有值的话新建
  */
@@ -33,6 +33,7 @@ public class Code01_TrieTree {
 
     public static class Trie1 {
         private Node1 root;
+
         public Trie1() {
             root = new Node1();
         }
@@ -41,7 +42,7 @@ public class Code01_TrieTree {
             char[] chars = insert.toCharArray();
             Node1 cur = root;
             cur.pass++;
-            for (int i = 0;i<chars.length;i++) {
+            for (int i = 0; i < chars.length; i++) {
                 int way = chars[i] - 'a';
                 if (cur.nexts[way] == null) {
                     cur.nexts[way] = new Node1();
@@ -53,6 +54,59 @@ public class Code01_TrieTree {
 //            cur = root;
         }
 
-//        public
+        // word这个单词之前有没有加入过(子串)
+        public boolean searchExist(String word) {
+            char[] chars = word.toCharArray();
+            Node1 cur = root;
+            for (int i = 0; i < chars.length; i++) {
+                int way = chars[i] - 'a';
+                if (cur.nexts[way] == null) {
+                    return false;
+                }
+                cur = cur.nexts[way];
+            }
+            return true;
+        }
+
+        // word这个单词加入过几次
+        public int search(String word) {
+            char[] chars = word.toCharArray();
+            Node1 cur = root;
+            for (int i = 0; i < chars.length; i++) {
+                int way = chars[i] - 'a';
+                if (cur.nexts[way] == null) {
+                    return 0;
+                }
+                cur = cur.nexts[way];
+            }
+            return cur.end;
+        }
+
+        // 删除Word
+        public boolean delete(String word) {
+            if (search(word) > 0) {
+                Node1 cur = root;
+                cur.pass--;
+                char[] chars = word.toCharArray();
+                for (int i = 0; i < word.length(); i++) {
+                    int index = chars[i] - 'a';
+//                    if (cur.nexts[index] != null) {
+//                        cur.pass--;
+//                    }
+//                    if (cur.nexts[index].pass == 0) {
+//                        cur.nexts[index] = null;
+//                        return true;
+//                    }
+
+                    if (--cur.nexts[index].pass == 0) {
+                        cur.nexts[index] = null;
+                        return true;
+                    }
+                }
+                cur.end--;
+                return true;
+            }
+            return false;
+        }
     }
 }
