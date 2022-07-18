9644f271c950c28b1084d941b022af33de3b554d
杨行满 <yanghm55@midea.com>
2022-07-18 18:04:53 +0800

    trie better

--- src/code08/Code01_TrieTree.java
+++ src/code08/Code01_TrieTree.java
@@ -8,6 +8,8 @@ package code08;
 
 import class03.Code03_StackAndQueueImpl;
 
+import java.util.HashMap;
+
 /**
  * 前缀树
  * <p>
@@ -109,6 +111,127 @@ public class Code01_TrieTree {
                 return true;
             }
             return false;
+        }
+
+
+
+
+
+    }
+
+    public static class Trie2 {
+
+        private static Node2 root;
+
+        Trie2(Node2 node2) {
+            root = new Node2();
+        }
+
+        /**
+         *
+         * Node1实现方式有很大的局限性
+         */
+        public static class Node2 {
+            private int pass;
+            private int end;
+            private HashMap<Integer, Node2> next;
+
+            Node2() {
+                next = new HashMap<>();
+            }
+        }
+
+        public static void insertNode2(String word) {
+            if (word == null) {
+                return;
+            }
+            Node2 cur = root;
+            cur.pass++;
+            char[] chars = word.toCharArray();
+            for (int i =0; i<chars.length;i++) {
+                int index = chars[i];
+                if (!cur.next.containsKey(index)) {
+                    cur.next.put(index,new Node2());
+                }
+                cur = cur.next.get(index);
+                cur.pass++;
+
+            }
+            cur.end++;
+        }
+
+        /**
+         * 完整的串加入了多少次
+         * @param word
+         * @return
+         */
+        public static int search(String word) {
+            if (word == null) {
+                return 0;
+            }
+            char[] chars = word.toCharArray();
+            Node2 cur = root;
+            for (int i =0;i<chars.length;i++) {
+                int index = chars[i] = 'a';
+                if (cur.next.get(index) == null) {
+                    return 0;
+                }
+                cur = cur.next.get(index);
+            }
+            return cur.end;
+
+        }
+
+        public static void delete(String word) {
+
+            if (search(word) > 0) {
+                Node2 cur = root;
+
+                char[] chars = word.toCharArray();
+                /**
+                 * 这种有问题
+                 * 假如节点来到倒数第二个位置
+                 * 此时无法将下一个数减1
+                 */
+//                for (int i =0;i< chars.length;i++) {
+//                    int index = chars[i] -'a';
+//                    cur.pass--;
+//                    cur = cur.next.get(index);
+//                    if (cur.pass== 0 ) {
+//                        cur.next = null;
+//                        break;
+//                    }
+//                }
+
+//                int index = 0;
+//                for (int i = 0; i < chars.length; i++) {
+//                    index = chars[i] - 'a';
+//                    cur.pass--;
+//                    cur = cur.next.get(index);
+//                    if (cur.pass == 0) {
+//                        cur.next.remove(index);
+//                        break;
+//                    }
+//                }
+//                cur.pass--;
+//                if (cur.pass == 0) {
+//                    cur.next.remove(index);
+//
+//                }
+//                cur.end--;
+
+                cur.pass--;
+                for (int i=0;i<word.length();i++) {
+                    int index = chars[i] -'a';
+                    if (--cur.next.get(index).pass ==0) {
+                        cur.next.remove(index);
+                        return;
+                    }
+                    cur = cur.next.get(index);
+                }
+                cur.end--;
+            }
+
         }
     }
 }
