96426e19c91ebedc86c62862ddce24520688060b
杨行满 <yanghm55@midea.com>
2022-07-19 20:07:34 +0800

    链表中点问题

---
+++ src/code09/Code01_LinkedListMid.java
@@ -0,0 +1,123 @@
+package code09;
+
+/**
+ * @Description
+ * @Author Scurry
+ * @Date 2022-07-19 10:52
+ */
+
+/**
+ * 快速排序时间常数好
+ * 堆排序空间省
+ * 归并排序稳定
+ * 时间复杂度o(n*logn) 空间复杂度o(n)不存在
+ */
+
+/**
+ * 链表问题：
+ * 解题方法论：
+ * 1.笔试 一切为了时间复杂度
+ * 2.面试 时间复杂度放在第一位 一定要找到空间最省的方法
+ */
+
+import class03.Code03_StackAndQueueImpl;
+import com.sun.org.apache.xalan.internal.xsltc.runtime.Node;
+
+/**
+ * 找到链表中点的位置
+ * 快慢指针 快指针一次走两步  慢指针一次走一步
+ */
+public class Code01_LinkedListMid {
+
+    public static class Node {
+        private int value;
+        private Node next;
+
+        Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 返回上中点
+     * 如果是偶数返回上中点
+     * 奇数返回中点
+     * 0 0 0 0 0 0
+     * @param head
+     * @return
+     */
+    public static Node midOrUpMidNode(Node head) {
+
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        Node slow = head.next;
+        Node fast = head.next.next;
+        while (fast.next !=null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 返回下中点
+     * 如果是偶数返回下中点
+     * 奇数返回中点
+     * 0 0 0 0 0 0
+     * @param head
+     * @return
+     */
+    public static Node midOrDownMidNode(Node head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node slow = head.next;
+        Node fast = head.next;
+        while (fast.next !=null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 上中点的前一个节点
+     * @return
+     */
+    public static Node midOrUpMidPreNode(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head.next.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 下中点的前一个节点
+     * @param head
+     * @return
+     */
+    public static Node midOrDownMidPreNode(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        if (head.next.next == null) {
+            return head;
+        }
+        Node slow = head;
+        Node fast = head.next;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
---
+++ src/code09/Code02_IsPalindromeList.java
@@ -0,0 +1,262 @@
+package code09;
+
+/**
+ * @Description
+ * @Author Scurry
+ * @Date 2022-07-19 14:38
+ */
+
+import java.util.Stack;
+
+/**
+ * 给定一个单链表头结点 哦按段该链表是否为回文结构
+ * 1.笔试用：链表入栈 出栈对比
+ * 2.面试用：找到上中点或者中点
+ */
+public class Code02_IsPalindromeList {
+
+    public static void printLinkedList(Node node) {
+        System.out.print("Linked List: ");
+        while (node != null) {
+            System.out.print(node.value + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Node head = null;
+        printLinkedList(head);
+//        System.out.print(isPalindromeList1(head) + " | ");
+//        System.out.print(isPalindromeList2(head) + " | ");
+//        System.out.println(isPalindromeList3(head) + " | ");
+//        printLinkedList(head);
+//        System.out.println("=========================");
+//
+//        head = new Node(1);
+//        printLinkedList(head);
+//        System.out.print(isPalindromeList1(head) + " | ");
+//        System.out.print(isPalindromeList2(head) + " | ");
+//        System.out.println(isPalindromeList3(head) + " | ");
+//        printLinkedList(head);
+//        System.out.println("=========================");
+//
+//        head = new Node(1);
+//        head.next = new Node(2);
+//        printLinkedList(head);
+//        System.out.print(isPalindromeList1(head) + " | ");
+//        System.out.print(isPalindromeList2(head) + " | ");
+//        System.out.println(isPalindromeList3(head) + " | ");
+//        printLinkedList(head);
+//        System.out.println("=========================");
+//
+//        head = new Node(1);
+//        head.next = new Node(1);
+//        printLinkedList(head);
+//        System.out.print(isPalindromeList1(head) + " | ");
+//        System.out.print(isPalindromeList2(head) + " | ");
+//        System.out.println(isPalindromeList3(head) + " | ");
+//        printLinkedList(head);
+//        System.out.println("=========================");
+//
+//        head = new Node(1);
+//        head.next = new Node(2);
+//        head.next.next = new Node(3);
+//        printLinkedList(head);
+//        System.out.print(isPalindromeList1(head) + " | ");
+//        System.out.print(isPalindromeList2(head) + " | ");
+//        System.out.println(isPalindromeList3(head) + " | ");
+//        printLinkedList(head);
+//        System.out.println("=========================");
+//
+//        head = new Node(1);
+//        head.next = new Node(2);
+//        head.next.next = new Node(1);
+//        printLinkedList(head);
+//        System.out.print(isPalindromeList1(head) + " | ");
+//        System.out.print(isPalindromeList2(head) + " | ");
+//        System.out.println(isPalindromeList3(head) + " | ");
+//        printLinkedList(head);
+//        System.out.println("=========================");
+//
+//        head = new Node(1);
+//        head.next = new Node(2);
+//        head.next.next = new Node(3);
+//        head.next.next.next = new Node(1);
+//        printLinkedList(head);
+//        System.out.print(isPalindromeList1(head) + " | ");
+//        System.out.print(isPalindromeList2(head) + " | ");
+//        System.out.println(isPalindromeList3(head) + " | ");
+//        printLinkedList(head);
+//        System.out.println("=========================");
+//
+//        head = new Node(1);
+//        head.next = new Node(2);
+//        head.next.next = new Node(2);
+//        head.next.next.next = new Node(1);
+//        printLinkedList(head);
+//        System.out.print(isPalindromeList1(head) + " | ");
+//        System.out.print(isPalindromeList2(head) + " | ");
+//        System.out.println(isPalindromeList3(head) + " | ");
+//        printLinkedList(head);
+//        System.out.println("=========================");
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(2);
+        head.next.next.next.next = new Node(1);
+        printLinkedList(head);
+        System.out.print(isPalindromeList1(head) + " | ");
+        System.out.print(isPalindromeList2(head) + " | ");
+        System.out.println(isPalindromeList3(head) + " | ");
+        printLinkedList(head);
+        System.out.println("=========================");
+
+    }
+
+    public static class Node {
+        private int value;
+        private Node next;
+        Node (int value) {
+            this.value = value;
+        }
+    }
+
+    /**
+     * 使用栈方式  时间复杂度O(N) 空间复杂度 O(N)
+     * @param head
+     * @return
+     */
+    public static boolean isPalindromeList1(Node head) {
+
+        if (head == null) {
+            return true;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        Node cur = head;
+        while (cur !=null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+
+        cur = head;
+        while (cur != null) {
+            if (cur.value != stack.pop().value) {
+                return false;
+            }
+            cur = cur.next;
+        }
+        return true;
+
+    }
+
+    /**
+     * 使用栈方式  后半部分入栈
+     * 时间复杂度O(N) 空间复杂度O(N/2)
+     * @param head
+     * @return
+     */
+    public static boolean isPalindromeList2(Node head) {
+
+        if (head == null) {
+            return true;
+        }
+
+        Node left = head;
+        Node right = head;
+        while (right.next != null && right.next.next!= null) {
+            left = left.next;
+            right = right.next.next;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        //慢指针来到中间位置 上中点
+        while (left.next !=null) {
+            stack.push(left.next);
+            left = left.next;
+        }
+        Node cur = head;
+        while (!stack.isEmpty()) {
+            if (cur.value != stack.pop().value) {
+                return false;
+            }
+            cur = cur.next;
+        }
+        return true;
+
+    }
+
+    /**
+     * 不使用栈方式  快慢指针
+     * 1.快慢指针来到中点位置
+     * 2.翻转后面的链表 依次对比
+     * 3.回复链表
+     * 时间复杂度O(N) 空间复杂度O(1)
+     * @param head
+     * @return
+     */
+    public static boolean isPalindromeList3(Node head) {
+
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        /**
+         * N N N N N N
+         */
+        Node left = head;
+        Node right = head;
+        while (right.next != null && right.next.next!= null) {
+            left = left.next;
+            right = right.next.next;
+        }
+
+        // 翻转后半部分链表
+        Node cur = left;
+        Node next;
+        Node pre = null;
+        // 1 2 3 2 1
+        while (cur != null) {
+            /**
+             * 不能提前打断下下个节点的联系
+             */
+//            next = cur.next;
+//            cur.next = pre;
+//            next.next = cur;
+//            pre = cur;
+//            cur = next;
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+
+        }
+
+        Node curNew = head;
+        Node curLeft = pre;
+        while (curLeft != null) {
+
+            if (curLeft.value != curNew.value) {
+                return false;
+            }
+            curLeft = curLeft.next;
+            curNew = head.next;
+        }
+
+        // 恢复链表
+        Node next2 = null;
+        while (pre != null) {
+             Node next1= pre.next;
+            pre.next = next2;
+             next2 = pre ;
+            pre = next1;
+
+        }
+
+        return true;
+
+    }
+}
