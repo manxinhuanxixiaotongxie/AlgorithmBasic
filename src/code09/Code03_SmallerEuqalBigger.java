74bf9a1cba65a9bad8ec963f62a6f9910925d9d1
yuyinweisan <>
2022-07-21 00:21:19 +0800

    链表的荷兰国旗问题

--- src/code09/Code03_SmallerEuqalBigger.java
+++ src/code09/Code03_SmallerEuqalBigger.java
@@ -12,11 +12,112 @@ package code09;
  */
 public class Code03_SmallerEuqalBigger {
 
-    public static void userArray(int[] arr){
+    static class Node {
+        private int value;
+        private Node next;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void useArray(int[] arr){
         int left = -1;
         int right = arr.length-1;
-        for (int i=0;i<arr.length-1;i++) {
-            if (arr[i] < arr[right])
+        int R = arr.length-1;
+        int i = 0;
+        while (i<right) {
+            if (arr[i] < arr[R]) {
+                swap(arr,++left,i++);
+            }
+            if (arr[i] == arr[R]) {
+                i++;
+            }
+            if (arr[i] > arr[R]) {
+                swap(arr,i,--right);
+            }
         }
+        swap(arr,R,right);
+    }
+
+    public static Node useNoArray(Node head,int value) {
+
+        Node sH = null;
+        Node sT = null;
+        Node eH = null;
+        Node eT = null;
+        Node bH = null;
+        Node bT = null;
+
+        Node next = null;
+        while (next != null) {
+            next = head.next;
+            if (head.value < value) {
+                if (sH == null) {
+                    sH = head;
+                    sT = head;
+                } else {
+                    sT.next = head;
+                    sT = head;
+                }
+            }
+
+            if (head.value == value) {
+                if (eH == null) {
+                    eH = head;
+                    eT = head;
+                }else {
+                    eT.next = head;
+                    eT = head;
+                }
+            }
+
+            if (head.value > value) {
+                if (bH == null) {
+                    bH = head;
+                    bT = head;
+                } else {
+                    bT.next = head;
+                    bT = head;
+                }
+            }
+
+            head = next;
+        }
+
+        /**
+         * 这样写有问题
+         * 可能没有==区域
+         */
+//        if (sH != null) {
+//            sT.next = eH;
+//
+//        }
+//
+//        if (eT != null) {
+//            eT.next = bH;
+//        }
+//        return sH;
+
+        if (sH != null) {
+//            sT.next = eH == null?bH:eH;
+            sT.next = eH;
+            eT = eT==null?sT:eT;
+        }
+
+        if (eT != null) {
+            eT.next = bH;
+
+        }
+        return sH != null ? sH : (eH != null ? eH : bH);
+
+
+    }
+
+    private static void swap(int[] arr,int a,int b) {
+
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+
     }
 }
