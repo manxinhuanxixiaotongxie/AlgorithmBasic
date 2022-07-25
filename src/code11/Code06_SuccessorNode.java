a6e7b26db25c3f11b22c42e042b77181692096d9
yuyinweisan <>
2022-07-25 22:22:33 +0800

    前驱节点后继节点

--- src/code11/Code06_SuccessorNode.java
+++ src/code11/Code06_SuccessorNode.java
@@ -41,12 +41,61 @@ public class Code06_SuccessorNode {
 
         if (cur.right!= null) {
             // 右树不为空
+            /**
+             * 右树不为空的时候 只要找到右树的最左节点就是后继节点
+             */
+            return getMostLeftNode(cur.right);
 
         }else {
+            /**
+             * 右树为空
+             * 向father方向查找
+             *
+             */
+
+            Node parent = cur.parent;
+
+            while (parent != null&& parent.left != cur ) {
+                cur = cur.parent;
+                parent = cur.parent;
+            }
+            return parent;
 
         }
+    }
+
+    public static Node getMostLeftNode(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
 
 
-        return new Node(1);
+
+    public static Node getPreNode(Node cur) {
+        if (cur == null) {
+            return null;
+        }
+
+        if (cur.left != null) {
+            /**
+             * 左子树不空   左子树的最右侧节点就是前驱节点
+             */
+            while (cur.right != null) {
+                cur = cur.right;
+            }
+            return cur;
+        } else {
+            Node parent = cur.parent;
+            while (parent != null && parent.right != cur) {
+                cur = cur.parent;
+                parent = cur.parent;
+            }
+            return parent;
+        }
     }
 }
