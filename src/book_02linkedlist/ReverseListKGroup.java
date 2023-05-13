ab5bdf4efd72e29f283fcfa40a5d223b6db7a98a
杨行满 <yanghm55@midea.com>
2023-05-13 16:18:24 +0800

    链表翻转问题以及字符串替换问题

---
+++ src/book_02linkedlist/ReverseListKGroup.java
@@ -0,0 +1,99 @@
+package book_02linkedlist;
+
+
+/**
+ * @Description leetcode测试通过
+ * @Author Scurry
+ * @Date 2023-05-13 10:23
+ */
+public class ReverseListKGroup {
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        reverseKGroup(n1,2);
+    }
+    /**
+     * leetcode k个一组翻转链表
+     * 给定一个链表
+     * 给定一个整数K
+     * 每K个进行反转
+     *
+     * 进阶：你可以设计一个O(1)额外内存空间的算法解决这个问题吗
+     */
+
+    public static ListNode reverseKGroup (ListNode head,int k) {
+
+        if (head == null || k <= 0) {
+            return head;
+        }
+
+        ListNode nextNode = getNextNode(head, k);
+        if (nextNode == null) {
+            return head;
+        }
+
+        ListNode listNode = reverseListNode(head, nextNode);
+
+        // 上一次结束end节点
+        ListNode lastEnd = head;
+
+        // 新头部
+        head = listNode;
+
+        ListNode next = null;
+        while (lastEnd != null) {
+            next = lastEnd.next;
+            ListNode nextNode1 = getNextNode(next, k);
+            if (nextNode1 == null) {
+                return head;
+            }
+            ListNode listNode1 = reverseListNode(next, nextNode1);
+            lastEnd.next = listNode1;
+            lastEnd = next;
+
+        }
+
+        return head;
+    }
+
+    /**
+     * start end进行翻转
+     *
+     * @param start
+     * @param end
+     * @return
+     */
+    public static ListNode reverseListNode(ListNode start , ListNode end) {
+
+        ListNode pre = null;
+        ListNode next = null;
+        end = end.next;
+        ListNode cur = start;
+        while (cur != end) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        start.next = end;
+        return pre;
+
+    }
+
+    public static ListNode getNextNode(ListNode start,int k) {
+        while (--k > 0 && start != null) {
+            start = start.next;
+        }
+        return start;
+    }
+
+
+}
---
+++ src/book_05string/StringChangeAndReplace.java
@@ -0,0 +1,39 @@
+package book_05string;
+
+/**
+ * @Description
+ * @Author Scurry
+ * @Date 2023-05-10 19:41
+ */
+public class StringChangeAndReplace {
+    /**
+     * 给定一个字符类型的数组
+     * 右半区全是空字符
+     * 左半区不含有空字符
+     *
+     * sothat
+     * 将左半区所有空格字符替换成“%20”
+     * 假设右半区足够大 可以满足替换所需要的空间
+     *
+     * 举例：
+     * "a b  c" 替换后 "a%20b%20%20c"
+     *
+     * 要求时间复杂度O(N)
+     * 空间复杂度O(1)
+     *
+     *
+     * 补充问题：
+     * 给定一个字符类型数组 只含有数字字符以及*
+     * 要求：把所有*移到数组左边- 数字移到数组右边
+     *
+     * 要求：时间复杂度O(N)
+     * 空间复杂度O(1)
+     * 不改变数组字符从左到有出现的顺序
+     *
+     */
+
+    public void stringChangeAndReplace(char[] chars) {
+
+
+    }
+}
