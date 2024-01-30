597fc70d2cd367079c3c5a67d7392f76fe68b868
yanghm55 <yanghm55@midea.com>
2024-01-30 11:44:19 +0800

    二叉树的深度优先遍历

--- src/systemimprove/code10/Code06_MaxChildBST.java
+++ src/systemimprove/code10/Code06_MaxChildBST.java
@@ -87,4 +87,109 @@ public class Code06_MaxChildBST {
     /**
      * 返回最大搜索二叉子树的头结点
      */
+    /**
+     * 分析可能性：
+     * 1.与X有关 左树是二叉搜索树右树是二叉搜索树 左树最大值小于X 右树最小值大于X  头结点就是X本身
+     * 2.与X无关
+     */
+    class Info2 {
+        TreeNode head;
+        boolean isBST;
+        int max;
+        int min;
+        int size;
+
+        Info2(TreeNode head, boolean isBST, int max, int min, int size) {
+            this.head = head;
+            this.isBST = isBST;
+            this.max = max;
+            this.min = min;
+            this.size = size;
+        }
+    }
+
+    public TreeNode getMaxBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        return process2(root).head;
+    }
+
+    public Info2 process2(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+        Info2 leftInfo = process2(node.left);
+        Info2 rightInfo = process2(node.right);
+        int max = node.val;
+        int min = node.val;
+        if (leftInfo != null) {
+            max = Math.max(max, leftInfo.max);
+            min = Math.min(min, leftInfo.min);
+        }
+        if (rightInfo != null) {
+            max = Math.max(max, rightInfo.max);
+            min = Math.min(min, rightInfo.min);
+        }
+        boolean isBST = false;
+        if (leftInfo != null && rightInfo != null) {
+            if (leftInfo.isBST && rightInfo.isBST && leftInfo.max < node.val && rightInfo.min > node.val) {
+                isBST = true;
+            }
+        }
+        int size = 0;
+        if (leftInfo != null && rightInfo != null) {
+            if (leftInfo.isBST && rightInfo.isBST && leftInfo.max < node.val && rightInfo.min > node.val) {
+                size = leftInfo.size + rightInfo.size + 1;
+            } else if (leftInfo.isBST && !rightInfo.isBST && leftInfo.max < node.val) {
+                size = leftInfo.size;
+            } else if (!leftInfo.isBST && rightInfo.isBST && rightInfo.min > node.val) {
+                size = rightInfo.size;
+            } else {
+                size = Math.max(leftInfo.size, rightInfo.size);
+            }
+        } else if (leftInfo != null) {
+            if (leftInfo.isBST && leftInfo.max < node.val) {
+                size = leftInfo.size + 1;
+            } else {
+                size = leftInfo.size;
+            }
+        } else if (rightInfo != null) {
+            if (rightInfo.isBST && rightInfo.min > node.val) {
+                size = rightInfo.size + 1;
+            } else {
+                size = rightInfo.size;
+            }
+        } else {
+            size = 1;
+        }
+
+        TreeNode head = null;
+        if (leftInfo != null && rightInfo != null) {
+            if (leftInfo.isBST && rightInfo.isBST && leftInfo.max < node.val && rightInfo.min > node.val) {
+                head = node;
+            } else if (leftInfo.isBST && !rightInfo.isBST && leftInfo.max < node.val) {
+                head = leftInfo.head;
+            } else if (!leftInfo.isBST && rightInfo.isBST && rightInfo.min > node.val) {
+                head = rightInfo.head;
+            } else {
+                head = leftInfo.size > rightInfo.size ? leftInfo.head : rightInfo.head;
+            }
+        } else if (leftInfo != null) {
+            if (leftInfo.isBST && leftInfo.max < node.val) {
+                head = node;
+            } else {
+                head = leftInfo.head;
+            }
+        } else if (rightInfo != null) {
+            if (rightInfo.isBST && rightInfo.min > node.val) {
+                head = node;
+            } else {
+                head = rightInfo.head;
+            }
+        } else {
+            head = node;
+        }
+        return new Info2(head, isBST, max, min, size);
+    }
 }
---
+++ src/systemimprove/code10/Code07_MaxLength.java
@@ -0,0 +1,43 @@
+package systemimprove.code10;
+
+public class Code07_MaxLength {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static int maxDistance2(Node head) {
+        return process(head).maxDistance;
+    }
+
+    public static class Info {
+        public int maxDistance;
+        public int height;
+
+        public Info(int m, int h) {
+            maxDistance = m;
+            height = h;
+        }
+
+    }
+
+    public static Info process(Node x) {
+        if (x == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        int p1 = leftInfo.maxDistance;
+        int p2 = rightInfo.maxDistance;
+        int p3 = leftInfo.height + rightInfo.height + 1;
+        int maxDistance = Math.max(Math.max(p1, p2), p3);
+        return new Info(maxDistance, height);
+    }
+}
