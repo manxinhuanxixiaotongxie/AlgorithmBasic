dc2b78b360b2898700e7a4bdbd13cb48910a21f8
yanghm55 <yanghm55@midea.com>
2024-02-18 11:19:40 +0800

    贪心最小字符串问题，放灯问题，其中 最小字符串实现的暴力递归有问题

--- src/systemimprove/code11/Code02_Light.java
+++ src/systemimprove/code11/Code02_Light.java
@@ -12,19 +12,19 @@ import java.util.HashSet;
  */
 public class Code02_Light {
 
-    public int getLights(String s) {
-        if (s== null || s.length() == 0) {
+    public static int getLights(String s) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
 
         char[] str = s.toCharArray();
-        return process(str,0,new HashSet<>());
+        return process(str, 0, new HashSet<>());
 
     }
 
-    public int process(char[] str, int index, HashSet<Integer> set) {
+    public static int process(char[] str, int index, HashSet<Integer> set) {
         if (index == str.length) {
-for (int i = 0; i < str.length; i++) {
+            for (int i = 0; i < str.length; i++) {
                 if (str[i] == '.') {
                     if (!set.contains(i - 1) && !set.contains(i) && !set.contains(i + 1)) {
                         return Integer.MAX_VALUE;
@@ -38,9 +38,87 @@ for (int i = 0; i < str.length; i++) {
             if (str[index] == '.') {
                 set.add(index);
                 yes = process(str, index + 1, set);
+                // 恢复现场
                 set.remove(index);
             }
             return Math.min(yes, no);
         }
+    }
+
+    public static String randomString(int len) {
+        char[] res = new char[(int) (Math.random() * len) + 1];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = Math.random() < 0.5 ? 'X' : '.';
+        }
+        return String.valueOf(res);
+    }
+
+    public static int minLight2(String road) {
+        char[] str = road.toCharArray();
+        int i = 0;
+        int light = 0;
+        while (i < str.length) {
+            if (str[i] == 'X') {
+                i++;
+            } else {
+                light++;
+                if (i + 1 == str.length) {
+                    break;
+                } else { // 有i位置 i+ 1 X .
+                    if (str[i + 1] == 'X') {
+                        i = i + 2;
+                    } else {
+                        i = i + 3;
+                    }
+                }
+            }
+        }
+        return light;
+    }
+
+    // 贪心解法
+    public static int getLights2(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] str = s.toCharArray();
+        int lights = 0;
+        int i = 0;
+        for (; i < str.length;) {
+            if (str[i] == '.') {
+                if (i + 1 == str.length) {
+                    lights++;
+                    break;
+                } else {
+                    if (str[i + 1] == 'X') {
+                        lights++;
+                        i = i + 2;
+                    } else {
+                        lights++;
+                        i = i + 3;
+                    }
+                }
+
+            }else {
+                i++;
+            }
+        }
+        return lights;
+    }
+
+    public static void main(String[] args) {
+        int len = 20;
+        int testTime = 100000;
+        for (int i = 0; i < testTime; i++) {
+            String test = randomString(len);
+            int ans1 = getLights(test);
+            int ans2 = minLight2(test);
+            int ans3 = getLights2(test);
+//            int ans3 = minLight3(test);
+            if (ans1 != ans2 || ans1 != ans3) {
+                System.out.println("oops!");
+            }
+        }
+        System.out.println("finish!");
     }
 }
---
+++ src/systemimprove/code11/Code03_GoldProblem.java
@@ -0,0 +1,65 @@
+package systemimprove.code11;
+
+public class Code03_GoldProblem {
+
+    // 暴力方法
+    public int minValue(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        return process(arr, 0);
+    }
+
+    // 暴力方法
+    public int process(int[] arr, int pre) {
+        // 将问题进行转化
+        if (arr.length == 1) {
+            return pre;
+        }
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                // 怎么进行转化呢？
+                // 金矿的拆分  ==》转化成金矿的合并
+                // 拆分所花费的数量与合并所花费的数量是一样的
+                // 那么合并花费的数量是多少呢？
+                // 之前已经合并了多少个金矿加上现在合并的两个金矿i和j
+                ans = Math.min(ans, process(copyAndMergeTwo(arr, i, j), pre + arr[i] + arr[j]));
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 此方法的含义是：
+     * 进行i位置与j位置进行合并
+     * 生成一个新的数组 是i位置与j位置合并与数组剩下的数组合成的一个新的数组
+     * int[] arrNew = new int[arr.length - 1];
+     * // 新数组位置的下标
+     * int ansi = 0;
+     * // 遍历原始数组
+     * for(int index = 0;index < arr.length;index++))   {
+     *     // 将i位置与j位置的元素进行合并
+     *     arrNew[ansi++] = arr[i] + arr[j];
+     *     // 其他位置维持原样
+     *     if (index != i && index != j) {
+     *         arrNew[ansi++] = arr[index];
+     *     }
+     * }
+     * @param arr
+     * @param i
+     * @param j
+     * @return
+     */
+    public int[] copyAndMergeTwo(int[] arr, int i, int j) {
+        int[] ans = new int[arr.length - 1];
+        int ansi = 0;
+        for (int arri = 0; arri < arr.length; arri++) {
+            if (arri != i && arri != j) {
+                ans[ansi++] = arr[arri];
+            }
+        }
+        ans[ansi] = arr[i] + arr[j];
+        return ans;
+    }
+}
