105a1185c3b3ed95d1e9d2abfedd6251abeec0e6
yanghm55 <yanghm55@midea.com>
2024-03-02 17:44:09 +0800

    单调栈结构以及斐波那契数列递推

--- src/systemimprove/code17/Code01_FiberArr.java
+++ src/systemimprove/code17/Code01_FiberArr.java
@@ -21,6 +21,36 @@ public class Code01_FiberArr {
 
     /**
      * 使用递推公式
+     * 斐波那契数列遵从严格的递推公式
+     * f(n) = f(n-1) + f(n-2)
+     * 中间没有任何的条件限制
+     * 因此使用
+     * 利用线性代数的方式：
+     * <p>
+     * 即如果存在F(n) = F(n-1) + F(n-2)这种严格意义的关系的话
+     * 那么他们的关系一定满足
+     * | F(n) ,F(n-1) | = | F(n-1), F(n-2) | * | {}1, 1}，{1,0}|
+     * <p>
+     * 我们利用这种关系找到一个快速的计算方式：
+     * | F(n) ,F(n-1) | = | F(n-1), F(n-2) | * | {}1, 1}，{1,0}|
+     * | F(n-1), F(n-2) | = | F(n-2), F(n-3) | * | {}1, 1}，{1,0}|
+     * | F(n-2), F(n-3) | = | F(n-3), F(n-4) | * | {}1, 1}，{1,0}|
+     * | F(n-3), F(n-4) | = | F(n-4), F(n-5) | * | {}1, 1}，{1,0}|
+     * | F(n-4), F(n-5) | = | F(n-5), F(n-6) | * | {}1, 1}，{1,0}|
+     * 。
+     * 。
+     * 。
+     * | F(2), F(1) | = | F(1), F(0) | * | {}1, 1}，{1,0}|
+     * <p>
+     * 那么可以得到| F(n) ,F(n-1) | = | F(1), F(0) | * | {}1, 1}，{1,0}| ^ (n-2)
+     * <p>
+     * <p>
+     * 现在就剩下一个问题：怎么快速求出| {}1, 1}，{1,0}| ^ (n-2)次方
+     * <p>
+     * 采用二进制二分的方式
+     * 见muliMatrix方法 与 getMatrix方法  需要注意的是单位矩阵是右边斜对角线为1的矩阵 其他位置为0
+     * <p>
+     * |
      */
     public int fiber2(int N) {
         if (N < 1) {
@@ -36,6 +66,7 @@ public class Code01_FiberArr {
 
     public int[][] getMatrix(int[][] base, int x) {
         int[][] ans = new int[base.length][base[0].length];
+        // 单位矩阵 此矩阵与任何矩阵相乘都是原矩阵
         for (int i = 0; i < ans.length; i++) {
             ans[i][i] = 1;
         }
@@ -44,6 +75,7 @@ public class Code01_FiberArr {
             if ((x & 1) != 0) {
                 ans = muliMatrix(ans, temp);
             }
+            // temp原地相乘
             temp = muliMatrix(temp, temp);
         }
         return ans;
