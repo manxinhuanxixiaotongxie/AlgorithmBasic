6d0eb653e60f799aa407317269c926028f94b9df
yanghm55 <yanghm55@midea.com>
2024-04-14 16:35:09 +0800

    使用sbt解决中位数

--- src/systemimprove/code28/Code01_CountOfRange.java
+++ src/systemimprove/code28/Code01_CountOfRange.java
@@ -4,6 +4,11 @@ import java.util.HashSet;
 
 /**
  * 给定一个数组 给定一个范围 返回数组中有多少个数在这个范围上
+ *
+ * 给定一个数组arr，和两个整数a和b（a<=b）
+ * 求arr中有多少个子数组，累加和在[a,b]这个范围上
+ * 返回达标的子数组数量
+ *
  */
 public class Code01_CountOfRange {
 
