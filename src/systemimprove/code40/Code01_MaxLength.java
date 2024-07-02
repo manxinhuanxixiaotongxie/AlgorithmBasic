e63d3041b711c353fc56ca9cf3a4a16f9b89c426
yanghm55 <yanghm55@midea.com>
2024-07-02 17:30:37 +0800

    数组==k的最大长度问题更新

--- src/systemimprove/code40/Code01_MaxLength.java
+++ src/systemimprove/code40/Code01_MaxLength.java
@@ -36,36 +36,36 @@ public class Code01_MaxLength {
             // R小于k的话 R向右移动
             if (sum < k) {
                 R++;
-                if (R >= arr.length) {
+                if (R == arr.length) {
                     break;
                 }
                 sum += arr[R];
             } else if (sum == k) {
                 ans = Math.max(ans, R - L);
                 R++;
-                if (R >= arr.length) {
+                if (R == arr.length) {
                     break;
                 }
                 sum += arr[R];
             } else {
                 // 如果sum的值已经比k大了，那么L向右移动
-                L++;
-                sum -= arr[L];
+                // 正数
+                sum -= arr[++L];
             }
         }
         return ans;
     }
 
-    public int[] generateArr(int maxValue,int maxLength) {
-        int[] ans = new int[(int) (Math.random()*maxLength+1)];
-        for (int i = 0; i < ans.length;i++) {
-            ans[i] = (int) (Math.random()*maxValue+1);
+    public int[] generateArr(int maxValue, int maxLength) {
+        int[] ans = new int[(int) (Math.random() * maxLength + 1)];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (int) (Math.random() * maxValue + 1);
         }
         return ans;
     }
 
-    public boolean isEqual(int[] arr1,int[] arr2) {
-        if (arr1== null && arr2 == null) {
+    public boolean isEqual(int[] arr1, int[] arr2) {
+        if (arr1 == null && arr2 == null) {
             return true;
         }
         if (arr1 == null || arr2 == null) {
@@ -74,7 +74,7 @@ public class Code01_MaxLength {
         if (arr1.length != arr2.length) {
             return false;
         }
-        for (int i = 0; i < arr1.length;i++) {
+        for (int i = 0; i < arr1.length; i++) {
             if (arr1[i] != arr2[i]) {
                 return false;
             }
@@ -87,11 +87,11 @@ public class Code01_MaxLength {
         int maxLength = 10;
         int testTimes = 100000;
         Code01_MaxLength code01_maxLength = new Code01_MaxLength();
-        for (int i = 0; i < testTimes;i++) {
-            int[] arr = code01_maxLength.generateArr(maxValue,maxLength);
-            int k = (int) (Math.random()*maxValue+1);
-            int ans1 = code01_maxLength.maxLength1(arr,k);
-            int ans2 = code01_maxLength.maxLength(arr,k);
+        for (int i = 0; i < testTimes; i++) {
+            int[] arr = code01_maxLength.generateArr(maxValue, maxLength);
+            int k = (int) (Math.random() * maxValue + 1);
+            int ans1 = code01_maxLength.maxLength1(arr, k);
+            int ans2 = code01_maxLength.maxLength(arr, k);
             if (ans1 != ans2) {
                 System.out.println("Oops!");
             }
