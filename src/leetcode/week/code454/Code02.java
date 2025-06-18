984e07be11c66ec58bc017d2d95cdab0e932b5a7
Scurry <770861530@qq.com>
2025-06-19 00:17:44 +0800

    454周赛

---
+++ src/leetcode/week/code454/Code02.java
@@ -0,0 +1,46 @@
+package leetcode.week.code454;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Code02 {
+    // 统计特殊三元组
+    public int specialTriplets(int[] nums) {
+        // 4  4 2 4 4
+        Map<Integer, Long> leftDoubleMap = new HashMap<>();
+        Map<Integer, Long> leftIndexMap = new HashMap<>();
+        leftDoubleMap.put(nums[0], 1L);
+        //从做到右遍历
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (leftDoubleMap.containsKey(nums[i] << 1)) {
+                leftIndexMap.put(i, leftDoubleMap.get(nums[i] << 1));
+            } else {
+                leftIndexMap.put(i, 0L);
+            }
+            leftDoubleMap.put(nums[i], leftDoubleMap.getOrDefault(nums[i], 0L) + 1);
+        }
+        // 右边
+        Map<Integer, Long> rightDoubleMap = new HashMap<>();
+        Map<Integer, Long> rightIndexMap = new HashMap<>();
+        rightDoubleMap.put(nums[nums.length - 1], 1L);
+        for (int i = nums.length - 2; i > 0; i--) {
+            if (rightDoubleMap.containsKey(nums[i] << 1)) {
+                rightIndexMap.put(i, rightDoubleMap.get(nums[i] << 1));
+            } else {
+                rightIndexMap.put(i, 0L);
+            }
+            rightDoubleMap.put(nums[i], rightDoubleMap.getOrDefault(nums[i], 0L) + 1);
+        }
+        long ans = 0;
+        for (int i = 1; i < nums.length - 1; i++) {
+            ans += leftIndexMap.get(i)* rightIndexMap.get(i) ;
+        }
+        // 帮我分析下这里为什么会出现负数
+        return (int) ((ans % 1000000007 + 1000000007) % 1000000007);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        System.out.println(new Code02().specialTriplets(nums));
+    }
+}
