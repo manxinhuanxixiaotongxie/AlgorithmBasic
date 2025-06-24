bff341e363f47a9f980c9cf3e0171597c035b88a
yanghm55 <yanghm55@mideqa.com>
2025-06-24 18:10:21 +0800

    159周第3题

--- src/leetcode/week/code159/Code03.java
+++ src/leetcode/week/code159/Code03.java
@@ -57,6 +57,39 @@ public class Code03 {
      *
      * 用两个窗口维护滑动窗口的最小质数与最大质数
      *
+     * 对于本题，我们需要知道上上个质数的位置 last2 上一个质数的位置last
+     *
+     * 当右端点固定为i时，左端点的合法范围为[left,last2]   采用滑动窗口能计算出[left,i]范围最大值与最小值
+
+
+     *
+     * [left,i]范围内维护一个窗口最大值、窗口最小值的更新结构
+     * 维护结束之后在[left,i]范围内的最大值和最小值的差小于等于k
+     *
+     * 怎么结算：
+     * left到i位置维护了窗口的单调结构
+     * 那么上上的 总共有多少个至少的范围内
+     * left-i 1个
+     * left+1-i 2个
+     * .
+     * .
+     * .
+     * last2-i i
+     * 总共有last2-left+1个合法的端点
+     *
+     * 那么只剩一个问题需要解决：
+     *
+     * last2与left的关系：
+     *
+     * 由于每次遇到质数才更新last2 left <=last2 +1恒成立
+     *
+     * 有：last2 - left + 1 个合法端点 加入答案
+     *
+     * 注当left = last2+1时 窗口中至多有一个质数 一定满足要求 所以left <= last2 + 1恒成立
+     *
+     * 即 last2 - left + 1 >= 0 恒成立 last2= -1 也满足这一个性质
+     *
+     *
      * @param nums
      * @param k
      * @return
@@ -66,12 +99,15 @@ public class Code03 {
 
         Deque<Integer> minQ = new ArrayDeque<>();
         Deque<Integer> maxQ = new ArrayDeque<>();
+        // 上上个质数
+        // 上个质数
         int last = -1, last2 = -1;
         int ans = 0, left = 0;
 
         for (int i = 0; i < nums.length; i++) {
             int x = nums[i];
 
+            // 是质数
             if (!NOT_PRIME[x]) {
                 // 1. 入
                 last2 = last;
@@ -87,9 +123,15 @@ public class Code03 {
                 }
                 maxQ.addLast(i);
 
+                // left到i位置维护的最大值 最小值的更新窗口
+                // 窗口维护是[left, i] 之间的最大值和最小值
+                // 如果[left, i]之间的最大值和最小值的差大于k
+                // 则需要移动左端点 left 说明窗口无法维持下去
+                // 经过这个步骤之后 窗口一定是满足最大值-最小值 <= k 的
                 // 2. 出
                 while (nums[maxQ.peekFirst()] - nums[minQ.peekFirst()] > k) {
                     left++;
+                    // 最大值、最小值的队列中如果有元素小于left，则出队 过期了
                     if (!minQ.isEmpty() && minQ.peekFirst() < left) {
                         minQ.pollFirst();
                     }
@@ -98,7 +140,10 @@ public class Code03 {
                     }
                 }
             }
-
+            // 至少包含两个质数是怎么保证的？
+            // 由于窗口每次只在最大最小质数差值合法时才统计答案，
+            // 且每次统计的子数组都包含至少两个质数(由last2控制) 所以不会多算或者算漏
+            // 滑动窗口保证了所有可能的区间都被遍遍历
             // 3. 更新答案
             ans += last2 - left + 1;
         }
