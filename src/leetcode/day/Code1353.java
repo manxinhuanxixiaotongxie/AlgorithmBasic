b109cce005812848007c223cf3ba40f27fac7ebc
yanghm55 <yanghm55@mideqa.com>
2025-07-07 17:55:32 +0800

    leetcode

--- src/leetcode/day/Code1353.java
+++ src/leetcode/day/Code1353.java
@@ -5,7 +5,59 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 
+/**
+ * 给你一个数组 events，其中 events[i] = [startDayi, endDayi] ，表示会议 i 开始于 startDayi ，结束于 endDayi 。
+ *
+ * 你可以在满足 startDayi <= d <= endDayi 中的任意一天 d 参加会议 i 。在任意一天 d 中只能参加一场会议。
+ *
+ * 请你返回你可以参加的 最大 会议数目。
+ *
+ */
 public class Code1353 {
+
+    /**
+     * 按开始时间分组+最小堆
+     *
+     * 1.第一天一定参加会议
+     *
+     * 2.参加结束时间最小的会议 参加结束时间最小的会议之后 问题变成从第二天开始 解决剩余n-1会议的子问题
+     *
+     *
+     * 需要知道哪些信息？
+     * 在第一天，可以参加哪些会议？
+     *
+     * 在第二天，可以参加哪些会议？
+     *
+     * 在第 i 天，可以参加哪些会议？
+     *
+     * 若按照开始时间分组，那么在第 i 天，开始时间为 i 的会议就是新增的可以参加的会议。
+     *
+     * 此外，还需要知道在剩余可以参加的会议中，结束时间最小的会议。根据观察二，参加这个会议。
+     *
+     * 算法
+     * 把会议按照开始时间分组，用 groups[i] 表示所有开始时间为 i 的会议的结束时间。
+     *
+     * 我们需要一个数据结构维护结束时间。这个数据结构需要支持如下操作：
+     *
+     * 添加结束时间。
+     * 查询结束时间的最小值。
+     * 删除最小的结束时间，表示我们参加这个会议，或者这个会议已过期（结束时间小于当前时间）。
+     * 最小堆完美符合要求。
+     *
+     * 在第 i 天：
+     *
+     * 删除最小堆中结束时间小于 i 的会议（已过期）。
+     * 把开始时间为 i 的会议的结束时间，加到最小堆中。
+     * 如果最小堆不为空，那么弹出堆顶（参加会议），把答案加一。
+     *
+     * 作者：灵茶山艾府
+     * 链接：https://leetcode.cn/problems/maximum-number-of-events-that-can-be-attended/solutions/3707151/liang-chong-fang-fa-zui-xiao-dui-bing-ch-ijbf/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param events
+     * @return
+     */
     public int maxEvents(int[][] events) {
         int mx = 0;
         for (int[] e : events) {
@@ -38,4 +90,5 @@ public class Code1353 {
         }
         return ans;
     }
+
 }
--- src/leetcode/practice/Code025.java
+++ src/leetcode/practice/Code025.java
@@ -70,26 +70,89 @@ public class Code025 {
         }
     }
 
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        if (head == null || k < 2) {
+            return head;
+        }
+        // k个一组翻转链表
+        int len = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            len++;
+            cur = cur.next;
+        }
+        // 如果长度小于k 不进行任何的操作
+        if (len < k) return head;
+        int index = 0;
+        cur = head;
+        // 上一组结束的点
+        ListNode pre = null;
+        // 下一组开始的点
+        ListNode next = null;
+        ListNode newHead = null;
+        while (cur != null) {
+            next = cur.next;
+            // k个一组
+            if (++index == k) {
+                // 翻转从pre到cur的链表
+                if (pre == null) {
+                    ListNode revserse = revserse(head, cur);
+                    newHead = revserse;
+                    pre = head;
+                    pre.next = next;
+                } else {
+                    // next已经有了 原来的pre.next节点作为新的pre
+                    ListNode curPre = pre;
+                    pre = curPre.next;
+                    ListNode revserse = revserse(curPre.next, cur);
+                    // 重新链接链表
+                    curPre.next = revserse;
+                    pre.next = next;
+                }
+                index = 0;
+            }
+            cur = next;
+        }
+
+        return newHead;
+
+    }
+
+    public ListNode revserse(ListNode begin, ListNode end) {
+        // 从pre到next翻转链表 返回新头部
+        ListNode cur = begin;
+        ListNode next = null;
+        ListNode pre = null;
+        while (cur != end) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        cur.next = pre;
+        return cur;
+    }
+
     public static void main(String[] args) {
-//        ListNode head = new ListNode(1);
-//        ListNode cur = head;
-//        cur.next = new ListNode(2);
-//        cur = cur.next;
-//        cur.next = new ListNode(3);
-//        cur = cur.next;
-//        cur.next = new ListNode(4);
-//        cur = cur.next;
-//        cur.next = new ListNode(5);
-//        cur = cur.next;
-//        cur.next = new ListNode(6);
         ListNode head = new ListNode(1);
         ListNode cur = head;
         cur.next = new ListNode(2);
         cur = cur.next;
         cur.next = new ListNode(3);
         cur = cur.next;
+        cur.next = new ListNode(4);
+        cur = cur.next;
+        cur.next = new ListNode(5);
+        cur = cur.next;
+//        cur.next = new ListNode(6);
+//        ListNode head = new ListNode(1);
+//        ListNode cur = head;
+//        cur.next = new ListNode(2);
+//        cur = cur.next;
+//        cur.next = new ListNode(3);
+//        cur = cur.next;
         Code025 code025 = new Code025();
-        ListNode newHead = code025.reverseKGroup(head, 2);
+        ListNode newHead = code025.reverseKGroup2(head, 2);
         while (newHead != null) {
             System.out.println(newHead.val);
             newHead = newHead.next;
