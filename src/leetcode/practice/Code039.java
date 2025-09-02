1258b4c5db247eea76d346cffb434933cb44bc4d
yanghm55 <yanghm55@mideqa.com>
2025-09-02 17:50:29 +0800

    leetcode100

--- src/leetcode/practice/Code039.java
+++ src/leetcode/practice/Code039.java
@@ -3,6 +3,10 @@ package leetcode.practice;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 这个解法是有问题的
+ * 报错 无法求解出答案
+ */
 public class Code039 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
@@ -22,6 +26,32 @@ public class Code039 {
             for (int i = index; i < candidates.length; i++) {
                 if (rest - candidates[i] >= 0) {
                     process(candidates, i, res, path + ',' + candidates[i], rest - candidates[i]);
+                }
+            }
+        }
+    }
+
+    List<List<Integer>> ans;
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        ans = new ArrayList<>();
+        process2(candidates, 0, target, new ArrayList<>());
+        return ans;
+    }
+
+    public void process2(int[] candidates, int index, int rest, List<Integer> cur) {
+        if (index == candidates.length) {
+            if (rest == 0) {
+                ans.add(new ArrayList<>(cur));
+            }
+        } else {
+            for (int zhang = 0; zhang * candidates[index] <= rest; zhang++) {
+                for (int i = 0; i < zhang; i++) {
+                    cur.add(candidates[index]);
+                }
+                process2(candidates, index + 1, rest - zhang * candidates[index], cur);
+                for (int i = 0; i < zhang; i++) {
+                    cur.remove(cur.size() - 1);
                 }
             }
         }
